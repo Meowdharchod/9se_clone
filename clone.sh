12ed894bb5fc73b1f56e89c751fa26dc9ceabc4f
Meowdharchod <107068268+Meowdharchod@users.noreply.github.com>
2023-08-23 17:31:48 +0530

    Update clone.sh

--- clone.sh
+++ clone.sh
@@ -1,6 +1,6 @@
 git clone https://github.com/Meowdharchod/android_device_realme_ice device/realme/ice
 git clone https://github.com/Meowdharchod/android_device_oneplus_sm8350-common device/oneplus/sm8350-common
-git clone https://github.com/Meowdharchod/vendor_realme_rmx3461 vendor/realme/RMX3461
 rm -rf vendor/firmware
 git clone https://gitlab.com/meowdharchod/proprietary_vendor_firmware.git vendor/firmware
 git clone https://gitlab.com/meowdharchod/vendor_realme_rmx3461 vendor/realme/RMX3461
+git clone https://github.com/Meowdharchod/proprietary_vendor_oneplus_sm8350-common vendor/oneplus/sm8350-common
