4e7d89309262cb9f844b90a51661e1d572e86ed8
Meowdharchod <107068268+Meowdharchod@users.noreply.github.com>
2023-08-29 23:59:30 +0530

    Update clone.sh

--- clone.sh
+++ clone.sh
@@ -6,3 +6,4 @@ git clone https://github.com/meowdharchod/vendor_realme_rmx3461 vendor/realme/RM
 git clone https://github.com/Meowdharchod/proprietary_vendor_oneplus_sm8350-common vendor/oneplus/sm8350-common
 git clone https://github.com/Meowdharchod/android_kernel_oplus_sm8350 kernel/oplus/sm8350
 git clone https://github.com/Meowdharchod/android_android_hardware_oplus hardware/oplus
+git clone https://github.com/LineageOS/android_hardware_lineage_livedisplay hardware/lineage/livedisplay
